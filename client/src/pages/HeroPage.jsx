254d434a33948fcd0fe523ddf61839eda3180915
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2024-10-03 11:18:54 -0400

    added hero page ui

--- client/src/pages/HeroPage.jsx
+++ client/src/pages/HeroPage.jsx
@@ -1,5 +1,17 @@
+import { Link } from "react-router-dom"
 const HeroPage = () => {
-    return <h1>Hero page</h1>
+    return (
+        <section className=' min-h-screen flex justify-center items-center flex-col w-[80%] m-auto'>
+            <article className='text-center mb-5'>
+                <h1 className='text-3xl font-bold tracking-tight mb-5 sm:text-5xl'>Welcome to ENTER NAME HERE</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet laudantium omnis rem voluptatum ipsa perspiciatis totam molestiae incidunt ad hic.</p>
+            </article>
+            <article>
+                <Link to='/login' className=' m-1 rounded-md bg-gray-900 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-gray-500'>Login</Link>
+                <Link to='/signup' className='m-1 rounded-md bg-gray-900 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-gray-500'>Signup</Link>
+            </article>
+        </section>
+    )
 }
 
 export default HeroPage
