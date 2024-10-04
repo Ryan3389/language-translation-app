8cf864932d55936377f0b9292451c80246cf0f85
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2024-10-04 11:04:02 -0400

    refactored login form

---
+++ client/src/components/FormComponent.jsx
@@ -0,0 +1,26 @@
+const FormComponent = ({ formTitle, fields, submitButtonText, formData, handleChange, handleFormSubmit }) => {
+
+    return (
+
+        <form onSubmit={handleFormSubmit} className='p-8 rounded-lg max-w-md w-full'>
+            <h1 className='text-3xl font-bold text-center'>{formTitle}</h1>
+            {fields.map((field) => (
+                <div key={field.name} className='mb-4'>
+                    <label htmlFor={field.name}>{field.label}</label>
+                    <input className='border border-gray-300 p-2 w-full rounded'
+                        type={field.type}
+                        name={field.name}
+                        id={field.name}
+                        placeholder={field.placeholder}
+                        value={formData[field.name]}
+                        onChange={handleChange}
+
+                    />
+                </div>
+            ))}
+            <input type="submit" value={submitButtonText} className='mt-4 w-full bg-gray-900 text-white py-2 rounded' />
+        </form>
+    )
+}
+
+export default FormComponent
