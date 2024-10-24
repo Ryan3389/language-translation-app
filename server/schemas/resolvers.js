80cff9093cefb1971fcb14dc4fb13501a43bd9d4
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2024-10-24 13:58:03 -0400

    fixed login bug and updated readme

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -26,7 +26,8 @@ const resolvers = {
     Mutation: {
         addUser: async (parent, { username, email, password }) => {
             try {
-                const user = await User.create({ email: email.trim().toLowerCase(), username, password });
+                // const user = await User.create({ email: email.trim().toLowerCase(), username, password });
+                const user = await User.create({ email: email.trim(), username, password });
 
 
                 const token = signToken(user)
